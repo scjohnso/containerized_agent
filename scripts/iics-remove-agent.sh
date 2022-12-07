7414142302d8d132881e8f1ecc4fe339efb84cab
BBird <bbird@informatica.com>
2022-12-07 07:23:29 -0500

    Adjust login script

--- scripts/iics-remove-agent.sh
+++ scripts/iics-remove-agent.sh
@@ -5,7 +5,7 @@ SESSION_ID=$(curl -X POST -H "Content-Type: application/json" -H "Accept: applic
 AGENT_ID=$(curl -H "icSessionid: $SESSION_ID" https://na1.dm-us.informaticacloud.com/saas/api/v2/agent/name/${1} | jq -r '.id')
 
 ### Try to limit the amount of delete commands issued immediately
-RAND=`shuf -i 1-7 -n 1`; sleep $RAND
+RAND=`shuf -i 1-11 -n 1`; sleep $RAND
 
 curl -X DELETE -H "icSessionid: $SESSION_ID" https://na1.dm-us.informaticacloud.com/saas/api/v2/agent/${AGENT_ID}
 
